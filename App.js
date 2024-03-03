53fcad59399e6c7ac40e24e6c2d7bd085f42dc80
Balazs Ferencz <ferenczbalazs85@gmail.com>
2024-03-03 08:39:07 +0000

    Initial commit - project setup and fonts

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider, useSafeAreaInsets  } from 'react-native-safe-area-context';
+import { useFonts } from 'expo-font';
+
+const HomeScreen = () => { 
+  const insets = useSafeAreaInsets();
+
+  const [fontsLoaded, fontError] = useFonts({
+    'PoppinsReg': require('./assets/fonts/Poppins-Regular.ttf'),
+    'PoppinsBlack': require('./assets/fonts/Poppins-Black.ttf'),
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
+  return(
+    <View style={{ flex: 1, paddingTop: insets.top, justifyContent: 'center', alignItems: 'center' }}>
+        <Text style={{ fontFamily: 'PoppinsReg', fontSize: 40,}}>Hello World!</Text>
+        <Text style={styles.container}>Hello World!</Text>
+        <StatusBar style="auto" />
+    </View>  
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <HomeScreen />
+    </SafeAreaProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    fontFamily: 'PoppinsBlack',  
+    fontSize: 40,
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
